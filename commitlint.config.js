2ee5b3eac1f76674958b62db149b26ece7647b87
Arrizo <arrizo787392502@163.com>
2023-05-10 22:31:34 +0800

    g

---
+++ commitlint.config.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: chenzechao
+ * @Date: 2023-05-10 22:30:48
+ * @LastEditTime: 2023-05-10 22:30:55
+ * @LastEditors: chenzechao
+ * @Description: 
+ * @FilePath: /sockets/commitlint.config.js
+ */
+module.exports = {
+  extends: [
+    'cz'
+  ]
+};
